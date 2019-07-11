43e7af4b95c9e095f98022440c121fb0ba068a5a
mohsen shokri hcs <mohsen shokri hcs>
2019-07-11 23:16:02 +0430

    linkshorter: now works

--- urlshorter/main.js
+++ urlshorter/main.js
@@ -1,11 +1,22 @@
 const Telegraf = require('telegraf')
+const request = require('request')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 start = ctx => ctx.reply('welcome to link shorter please send /help for more information!')
-idk = ctx => ctx.reply('i dont know what you say i am a bot')
+idk = ctx => ctx.reply(ctx.message.text)
 help = ctx => ctx.reply('send /short {url} to short your url')
 
-short = ctx => ctx.reply('please wait...')
+url = 'https://2ad.ir/api?api=34e60586c43caec7111f3ac3b3e60fa1fb5a9eb8&url='
+
+
+short = ctx =>{
+    ctx.reply('please wait...')
+    text = ctx.message.text
+    url = url+text.slice(7)
+    response = (err,resp,body) => (!err && resp.statusCode ==200)?ctx.reply(JSON.parse(resp.body)['shortenedUrl']):console.log(err)
+    request.get(url,response)
+   
+}
 
 bot.start(start)
 bot.help(help)
